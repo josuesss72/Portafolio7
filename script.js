aa38c8d241dfe5fb67b98f0eba13ffe0c35ec1bf
josuesss72 <jjdanielsarmiento@gmail.com>
2022-11-03 20:12:55 -0500

    close Menu

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ const iconMenu = document.querySelector('.btn')
 const menu = document.querySelector('.header')
 const iconX = document.querySelector('.bx-x')
 const iconH = document.querySelector('.bx-menu')
+const boxSectionMenu = document.querySelector('#box_section_menu')
 
-console.log(iconX)
+
 iconMenu.addEventListener('click', function () {
 	menu.classList.toggle('menu_show')
 	iconH.classList.toggle('bx-menu_disguise')
 	iconX.classList.toggle('bx-menu_disguise')
 })
+
+boxSectionMenu.addEventListener('click', function(e){
+	if(e.target.classList.contains('close')){
+		menu.classList.toggle('menu_show')
+		iconH.classList.toggle('bx-menu_disguise')
+		iconX.classList.toggle('bx-menu_disguise')
+	}
+})
+
